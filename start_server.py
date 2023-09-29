d79fa33d61d0bcf9c63b667b0ce36a7808417419
Andrew Santhosh C <andrewsanthosh264@gmail.com>
2023-09-29 13:16:17 +0530

    initial flasking

---
+++ start_server.py
@@ -0,0 +1,51 @@
+import os
+import shutil
+
+import flask
+from flask import request, render_template, send_from_directory
+
+app = flask.Flask(__name__)
+
+chat_messages = []
+app.config['UPLOAD_FOLDER'] = 'files'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', chat_messages=chat_messages)
+
+
+@app.route('/<path:name>')
+def download_file(name):
+    return send_from_directory(
+        app.config['UPLOAD_FOLDER'], name, as_attachment=True
+    )
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    message = request.form.get('message', '')
+
+    if 'image' in request.files:
+        image = request.files['image']
+
+        if image.filename != '':
+            # generate a random filename
+            file_format = image.filename.split('.')[-1]
+            filename = f'{os.urandom(16).hex()}.{file_format}'
+            image.save(os.path.join('./files', filename))
+            chat_messages.append(('user', message, filename))
+        else:
+            chat_messages.append(('user', message, ''))
+
+    return render_template('index.html', chat_messages=chat_messages)
+
+
+if __name__ == '__main__':
+
+    if os.path.exists('./files'):
+        shutil.rmtree('./files')
+
+    os.mkdir('./files')
+
+    app.run(port=5000, debug=True)
