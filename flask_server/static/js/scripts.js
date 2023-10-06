74eb0debb6c3cc2ea28819e491474f935ef39b5a
ShadowConYT <iamajay6122@gmail.com>
2023-10-06 16:23:13 +0530

    Lanuage added

--- flask_server/static/js/scripts.js
+++ flask_server/static/js/scripts.js
@@ -17,3 +17,99 @@ const selectedElem = document.querySelector(`.lang.${selectedLang}`);
 selectedElem.style.display = 'block';
 });
 
+
+// JavaScript code to handle user input and display chat
+const chatContainer = document.getElementById('chat');
+const chatForm = document.getElementById('chat-form');
+const userInput = document.getElementById('message');
+const  image = document.getElementById('image');
+
+
+chatForm.addEventListener('submit', (e) => {
+e.preventDefault();
+const userMessage = userInput.value;
+appendMessage('User', userMessage);
+
+const imageFile = image.files[0];
+if (imageFile) {
+const reader = new FileReader();
+reader.onload = (event) => {
+const imageSrc = event.target.result;
+appendImage('User', imageSrc);
+fetchBotResponse(userMessage, imageSrc)
+.then(botResponse => {
+        appendMessage('Bot', botResponse);
+        chatForm.reset(); // Reset the form after the bot response is received
+    })
+.catch(error => {
+    console.error(error);
+});
+};
+reader.readAsDataURL(imageFile);
+} else {
+fetchBotResponse(userMessage)
+.then(botResponse => {
+    appendMessage('Bot', botResponse);
+    chatForm.reset(); // Reset the form after the bot response is received
+})
+.catch(error => {
+    console.error(error);
+});
+}
+});
+
+function appendMessage(sender, message) {
+const messageElement = document.createElement('div');
+messageElement.textContent = `${sender}: ${message}`;
+chatContainer.appendChild(messageElement);
+}
+
+function appendImage(sender, imageSrc) {
+const imageElement = document.createElement('img');
+imageElement.src = imageSrc;
+imageElement.alt = `${sender}'s image`;
+imageElement.width = 200;
+imageElement.height = 200;
+chatContainer.appendChild(imageElement);
+}
+
+
+function fetchBotResponse(userMessage, imageSrc) {
+// Send the user's message and image to the server for processing (via AJAX or WebSocket)
+// Upon receiving a response from your chatbot, append it to the chat
+// You can use AJAX or WebSocket to asynchronously communicate with the server
+// and append the bot's response to the chat container.
+// Here's a simplified example:
+
+// Assume you have a function fetchBotResponse() that fetches the response from the server
+// and returns a Promise that resolves with the bot's response
+fetchBotResponseFromServer(userMessage, imageSrc)
+.then(botResponse => {
+appendMessage('Bot', botResponse);
+})
+.catch(error => {
+console.error(error);
+});
+}
+
+function fetchBotResponseFromServer(userMessage, imageSrc) {
+// Use fetch() or WebSocket to send the user's message and image to the server
+// and receive the bot's response
+// Here's a simplified example using fetch():
+const formData = new FormData();
+formData.append('message', userMessage);
+if (imageSrc) {
+formData.append('image', imageSrc);
+}
+return fetch('/chatbot', {
+method: 'POST',
+body: formData
+})
+.then(response => {
+if (!response.ok) {
+throw new Error('Failed to fetch bot response');
+}
+return response.text();
+});
+}
+
