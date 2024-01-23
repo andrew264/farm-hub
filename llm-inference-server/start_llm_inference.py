45171b6343065474e5b0c72eca86e1a805108e80
Andrew Santhosh C <andrewsanthosh264@gmail.com>
2024-01-23 22:48:01 +0530

    moving from ctransformers to vllm

--- llm-inference-server/start_llm_inference.py
+++ llm-inference-server/start_llm_inference.py
@@ -3,11 +3,13 @@ import json
 import signal
 
 import websockets
-from ctransformers import AutoModelForCausalLM
+from vllm import AsyncEngineArgs, AsyncLLMEngine, SamplingParams
 
 from types_and_constants import WS_EOS
 
-llm = None
+engine = None
+path = "/mnt/Ddrive/Llama-2-7B-Chat-AWQ"
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=256)
 
 
 async def handle_client(websocket: websockets.WebSocketServerProtocol, path: str = None):
@@ -19,11 +21,11 @@ async def handle_client(websocket: websockets.WebSocketServerProtocol, path: str
 
             generated = ''
             # Send the input to the model and get the output
-            for token in llm(client_input, stream=True,
-                             top_p=0.9, temperature=1.2, batch_size=1,
-                             max_new_tokens=756, stop=['</s>']):
-                generated += token
-                await websocket.send(token)
+            async for output in engine.generate(client_input, sampling_params, request_id="INFERENCE-SERVER"):
+                new_generated = output.outputs[0].text.replace(generated, "")
+                await websocket.send(new_generated)
+                generated = output.outputs[0].text
+            await websocket.send(WS_EOS)
 
             # send eos
             pong_waiter = await websocket.ping()
@@ -51,10 +53,8 @@ async def start_server(host: str, port: int):
 
 if __name__ == "__main__":
     print("Loading LLM...")
-    llm = AutoModelForCausalLM.from_pretrained("Bloke/model.gguf",
-                                               model_type="llama",
-                                               context_length=4096,
-                                               gpu_layers=30, )
+    engine_args = AsyncEngineArgs(model=path, max_model_len=1440, quantization="awq", block_size=8)
+    engine = AsyncLLMEngine.from_engine_args(engine_args)
     print("Starting server...")
     with open('config.json') as f:
         config = json.load(f)
